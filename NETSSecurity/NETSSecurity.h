d091c8607ce7176a25044cc518eb7e66ddb6b54e
Tun Lin <tunlin@nets.com.sg>
2017-05-11 16:02:54 +0800

    Initial Commit

---
+++ NETSSecurity/NETSSecurity.h
@@ -0,0 +1,19 @@
+//
+//  NETSSecurity.h
+//  NETSSecurity
+//
+//  Created by Innovation on 5/11/17.
+//  Copyright © 2017 NETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NETSSecurity.
+FOUNDATION_EXPORT double NETSSecurityVersionNumber;
+
+//! Project version string for NETSSecurity.
+FOUNDATION_EXPORT const unsigned char NETSSecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NETSSecurity/PublicHeader.h>
+
+
